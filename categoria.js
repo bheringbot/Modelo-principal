95c05dc920541cd70ecd0c316a276a3892ad89f4
bheringbot <bheringkaua644@gmail.com>
2025-06-26 13:26:44 -0300

    Create categoria.js

---
+++ categoria.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("categoria-container");
+
+fetch("data/categoria.json")
+  .then(response => response.json())
+  .then(categorias => {
+    categorias.forEach(cate => {
+      const card = document.createElement("div");
+      card.className = "card-categoria";
+      card.innerHTML = `
+        <img src="${cate.imagem}" alt="${cate.nome}">
+        <span>${cate.nome}</span>
+      `;
+      container.appendChild(card);
+    });
+  })
+  .catch(error => {
+    console.error("Erro ao carregar categorias:", error);
+  });
