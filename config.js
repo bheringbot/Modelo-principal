30cbc716d9d95777a8fb1aa4ac826e9bc78116b3
bheringbot <bheringkaua644@gmail.com>
2025-06-27 19:10:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // config.js
 const config = {
-  botName: "LinkZap Bot",
-  temaCor: "darkturquoise",
-  mensagemPadrao: "Olá! Vim pelo seu site.",
-  linkWhatsBase: "https://wa.me/"
+  botName: "MyCodes",
+  
 };
