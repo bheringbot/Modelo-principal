73301a391d01d3d8b951d4cc9d8b0fc14d1e73eb
bheringbot <bheringkaua644@gmail.com>
2025-06-27 15:48:09 -0300

    Update categoria.js

--- categoria.js
+++ categoria.js
@@ -6,6 +6,8 @@ fetch("data/categoria.json")
     categorias.forEach(cate => {
       const card = document.createElement("div");
       card.className = "card-categorias";
+      card.setAttribute("data-categoria", cate.nome); // [1]
+card.addEventListener("click", () => filtrarPorCategoria(cate.nome)); // [2]
       card.innerHTML = `
         <img src="${cate.imagem}" alt="${cate.nome}">
         <span>${cate.nome}</span>
